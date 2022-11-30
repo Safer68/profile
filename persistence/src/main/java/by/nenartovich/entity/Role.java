d92ee46a3b460d1b64d8ca77a46d920e437d1648
Safer68 <60327596+Safer68@users.noreply.github.com>
2022-12-01 01:22:13 +0300

    Initial commit

--- persistence/src/main/java/by/nenartovich/entity/Role.java
+++ persistence/src/main/java/by/nenartovich/entity/Role.java
@@ -1,2 +1,17 @@
-package by.nenartovich.entity;public class Role {
+package by.nenartovich.entity;
+
+import lombok.Data;
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", length = 50)
+    private String name;
 }
